160b09c01c19537e9ae611a30280a661dbf21554
Elodie D <elodie.deflon@gmail.com>
2022-02-17 16:41:50 +0100

    Update spyned.py
    Client fonctionnel avec "addition"
    A corriger pour faire fonctionner avec "VehicleList"

--- spyned.py
+++ spyned.py
@@ -1,8 +1,8 @@
 import json
 
-from lxml.builder import unicode
-from spyne import Application, Unicode, rpc, Service, AnyDict
+from spyne import Application, rpc, Service, AnyDict, Integer
 from spyne.protocol.soap import Soap11
+from spyne.protocol.xml import XmlDocument
 from spyne.server.wsgi import WsgiApplication
 
 
@@ -27,30 +27,31 @@ class ServiceVehicles(Service):
 
     # @brief : Return all vehicle of the list
     # @return : list of dictionary
-    @rpc(_returns=AnyDict)
-    def get_vehicles(self):
-        list_vehicles = init_list_vehicles()
-        return list_vehicles
+    # @rpc(_returns=AnyDict)
+    # def get_vehicles(self):
+    #     return init_list_vehicles()
 
     # @rpc(Unicode, Integer, _returns=Iterable(Unicode))
     # def say_hello(self, name, times):
     #     for i in range(times):
     #         yield u'Hello, %s' % name
     #
-    # @rpc(Integer, Integer, _returns=Integer)
-    # def addition(self, a, b):
-    #     return a + b
+
+    @rpc(Integer, Integer, _returns=Integer)
+    def addition(self, a, b):
+        return a + b
+
+
+application = Application(
+    services=[ServiceVehicles],
+    tns='http://tests.python-zeep.org/',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11())
+
+wsgi_application = WsgiApplication(application)
 
 
 if __name__ == '__main__':
-    application = Application(
-        services=[ServiceVehicles],
-        tns='http://tests.python-zeep.org/',
-        in_protocol=Soap11(validator='lxml'),
-        out_protocol=Soap11())
-
-    application = WsgiApplication(application)
-
     import logging
 
     from wsgiref.simple_server import make_server
@@ -61,5 +62,5 @@ if __name__ == '__main__':
     logging.info("listening to http://127.0.0.1:8000")
     logging.info("wsdl is at: http://localhost:8000/?wsdl")
 
-    server = make_server('127.0.0.1', 8000, application)
+    server = make_server('127.0.0.1', 8000, wsgi_application)
     server.serve_forever()
