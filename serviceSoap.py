1c42ebdc462a0471ce27a8ef9010a5a0d6cfbc0c
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-02-18 13:39:42 +0100

    Deploy commit

--- serviceSoap.py
+++ serviceSoap.py
@@ -3,6 +3,7 @@ import json
 from spyne import Application, rpc, Service, Iterable, Unicode
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
+import os
 
 from serviceVehicles import VehicleList, Vehicle
 
@@ -79,8 +80,9 @@ if __name__ == '__main__':
     url = '0.0.0.0'
     port = 35000
 
+
     logging.info(f"listening to http://{url}:{port}")
     logging.info(f"wsdl is at: http://{url}:{port}/?wsdl")
 
-    server = make_server(url, port, wsgi_application)
+    server = make_server(url, os.environ.get("PORT", port), wsgi_application)
     server.serve_forever()
